609920415615067048dee156a7098c4bf17e07b3
Jonathan Noguerales <jonathannoguerales@gmail.com>
2022-07-06 19:27:51 +0200

    1

---
+++ src/pages/Pdf.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Pdf extends Component {
+
+    componentDidMount() {
+        if(typeof window.orientation !== "undefined"){
+            document.getElementById('enlaceDescargarPdf').click();
+            window.close();
+        }
+    }
+    
+    render() {
+        return (
+            <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+                <object
+                data={require('../curriculum/curriculum.pdf')}
+                type="application/pdf"
+                width="100%"
+                height="100%"
+                >
+                    <br />
+                    <a href={require('../curriculum/curriculum.pdf')} id="enlaceDescargarPdf"
+                    download="ReactJS.pdf"
+                    >Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo</a>
+                </object>
+            </div>
+        );
+    }
+}
+
+export default Pdf;
---
+++ src/routes/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from '../pages/App';
+import Pdf from '../pages/Pdf';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>}/>
+        <Route exact path="/pdf" element={<Pdf/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
