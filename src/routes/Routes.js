faa359d20a97ddbd7fed59d1be4f3718d57403e6
Jonathan Noguerales <jonathannoguerales@gmail.com>
2022-07-14 15:58:48 +0200

    4.0

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../App';
 import Pdf from '../pages/Pdf';
 
 function AppRouter() {
+
+  const [loading, setLoading] = useState(true)
+  const spinner = document.getElementById('spinner')
+
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = 'none';
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<App/>}/>
-        <Route exact path="/pdf" element={<Pdf/>}/>
-      </Routes>
-    </Router>
-  );
+    !loading && (
+      <>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route exact path="/pdf" element={<Pdf />} />
+          </Routes>
+        </Router>
+      </>)
+    );
 }
 
 export default AppRouter;
